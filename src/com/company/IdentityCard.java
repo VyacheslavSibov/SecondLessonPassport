83b183dd143657d5d09b81368c9344bef4ecfe4d
sea_odua <begineicht@icloud.com>
2022-06-12 16:12:52 +0300

    FirstCommit

---
+++ src/com/company/IdentityCard.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class IdentityCard {
+        String number;
+
+        String photo;
+        String lastname;
+        String firstname;
+        String middlename;
+        String signature;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        IdentityCard passport = new IdentityCard();
+        passport.number = "SeriaPassport-Number";
+        passport.photo = "Face";
+        passport.lastname = "Vyacheslav";
+        passport.firstname = "Sibov";
+        passport.middlename = "Ivanovich";
+        passport.signature = "signature";
+
+
+        System.out.println(passport.number);
+        System.out.println(passport.photo);
+        System.out.println(passport.firstname);
+        System.out.println(passport.lastname);
+        System.out.println(passport.middlename);
+        System.out.println(passport.signature);
+        }
+    }
